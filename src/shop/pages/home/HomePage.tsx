4a9f70de5cb86921f52aede3377ed29676c7e6c8
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-03-10 17:11:57 -0600

    feat: HomePage pagination and products

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { Pagination } from "@/components/custom/Pagination";
+import { products } from "@/mocks/products.mock";
 import { Jumbotron } from "@/shop/components/Jumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <Jumbotron title="Todos los productos" />
+
+      <ProductsGrid products={products} />
+
+      <Pagination totalPages={7} />
     </>
   );
 };
