cf3f6b323790088afa39b493420f6908b787449f
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-03-12 11:37:55 -0600

    feat: navigate between screens

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,21 @@
+import { Pagination } from "@/components/custom/Pagination";
+import { products } from "@/mocks/products.mock";
+import { Jumbotron } from "@/shop/components/Jumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+  return (
+    <>
+      <Jumbotron title={`Productos para ${genderLabel}`} />
+
+      <ProductsGrid products={products} />
+
+      <Pagination totalPages={7} />
+    </>
+  );
 };
