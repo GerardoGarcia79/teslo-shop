fb2cb16d036e03540c45e5513c2737cc3b90834a
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-03-26 12:54:10 -0600

    feat: Check auth status

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,18 +1,41 @@
 import { RouterProvider } from "react-router";
+import type { PropsWithChildren } from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 
 import { appRouter } from "./app.router";
+import { checkAuthAction } from "./auth/actions/check-auth.action";
+import { FullScreenLoading } from "./components/custom/FullScreenLoading";
 
 const queryClient = new QueryClient();
+
+const CustomAuthProvider = ({ children }: PropsWithChildren) => {
+  const { isLoading } = useQuery({
+    queryKey: ["auth"],
+    queryFn: checkAuthAction,
+    retry: false,
+    refetchInterval: 1000 * 60 * 90, // 90 minutes,
+    refetchOnWindowFocus: true,
+  });
+
+  if (isLoading) return <FullScreenLoading />;
+
+  return children;
+};
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <RouterProvider router={appRouter} />
+      <CustomAuthProvider>
+        <RouterProvider router={appRouter} />
+      </CustomAuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
