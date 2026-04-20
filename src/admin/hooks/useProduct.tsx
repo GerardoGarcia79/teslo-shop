30ddc33389c363f6692a500efb597fdfcee8c883
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-04-20 15:52:50 -0600

    feat: Create product

--- src/admin/hooks/useProduct.tsx
+++ src/admin/hooks/useProduct.tsx
@@ -1,8 +1,10 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getProductByIdAction } from "../actions/get-product-by-id.action";
 import { createUpdateProductAction } from "../actions/create-update-product.action";
 
 export const useProduct = (id: string) => {
+  const queryClient = useQueryClient();
+
   const data = useQuery({
     queryKey: ["product", { id }],
     queryFn: () => getProductByIdAction(id),
@@ -14,7 +16,13 @@ export const useProduct = (id: string) => {
   const mutation = useMutation({
     mutationFn: createUpdateProductAction,
     onSuccess: (product) => {
-      console.log("Todo salió bien", product);
+      // Invalidad caché
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      queryClient.invalidateQueries({
+        queryKey: ["product", { id: product.id }],
+      });
+      // Actualizar queryData
+      queryClient.setQueryData(["product", { id: product.id }], product);
     },
   });
 
